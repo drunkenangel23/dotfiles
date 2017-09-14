6235f3b6822738bae1be1128c92fe0c39211df36
Brad Kusy <brad.kusy@activision.com>
2017-09-14 09:25:30 -0700

    added skip checks

--- make_links.bat
+++ make_links.bat
@@ -13,11 +13,26 @@ if not exist %USERPROFILE%\vimfiles junction %USERPROFILE%\vimfiles %USERPROFILE
 rem mintty is less than stellar and ignores the config file unless it has unix line endings.
 rem git is a pain in the ass and insists on fucking with the line endings so just copy the 
 rem file.  Afterwards, you will likely have to to a "set ff=unix" in vim to fix it up.
+if exist %APPDATA%\mintty goto HAVE_MINTTY
 mkdir %APPDATA%\mintty
-copy /y %USERPROFILE\.dotfiles\config\mintty\config %APPDATA%\mintty\config
+:HAVE_MINTTY
+if exist %APPDATA%\mintty\config goto SKIP_MINTTY_CONFIG
+copy /y %USERPROFILE%\.dotfiles\config\mintty\config %APPDATA%\mintty\config
+goto HAVE_MINTTY_CONFIG
+:SKIP_MINTTY_CONFIG
+echo %APPDATA%\mintty\config already exists.  Skipped.
+:HAVE_MINTTY_CONFIG
 
+if exist %USERPROFILE%\.config goto HAVE_CONFIG
 mkdir %USERPROFILE%\.config\
+:HAVE_CONFIG
+
+if exist %USERPROFILE%\.config\git goto SKIP_CONFIG_GIT
 junction %USERPROFILE%\.config\git %USERPROFILE%\.dotfiles\config\git
+goto HAVE_CONFIG_GIT
+:SKIP_CONFIG_GIT
+echo %USERPROFILE%\.config\git already exists.  Skipped.
+:HAVE_CONFIG_GIT
 
 
 
