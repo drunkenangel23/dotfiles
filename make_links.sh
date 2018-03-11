9a3254efb70384c3a1af9a2d3cc8b18b6507364f
Brad Kusy <brad.kusy@gmail.com>
2018-03-11 08:58:03 -0700

    prune unix links

--- make_links.sh
+++ make_links.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-ln -s ~/.dotfiles/bashrc ~/.bashrc
-ln -s ~/.dotfiles/bash_profile ~/.bash_profile
-ln -s ~/.dotfiles/iterm2 ~/.iterm2
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/gitexcludes ~/.gitexcludes
-ln -s ~/.dotfiles/vscode_user_settings.json ~/Library/Application\ Support/Code/User/settings.json
+#ln -s ~/.dotfiles/bashrc ~/.bashrc
+#ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+#ln -s ~/.dotfiles/iterm2 ~/.iterm2
+#ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+#ln -s ~/.dotfiles/gitexcludes ~/.gitexcludes
+#ln -s ~/.dotfiles/vscode_user_settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 if [ ! -d ~/.vim ]; then
     ln -s ~/.dotfiles/vim ~/.vim
 fi
 
-mkdir -p ~/.config
-ln -s ~/.dotfiles/config/nvim ~/.config
+#mkdir -p ~/.config
+#ln -s ~/.dotfiles/config/nvim ~/.config
 
-mkdir -p ~/.local/share
-ln -s ~/.dotfiles/local/share/nvim ~/.local/share
+#mkdir -p ~/.local/share
+#ln -s ~/.dotfiles/local/share/nvim ~/.local/share
 
-mkdir -p ~/.local/lib
-case $OSTYPE in
-    darwin*)
-        ln -s /usr/local/Cellar/llvm/HEAD-600899c/lib ~/.local/lib/llvm ;;
-esac
+#mkdir -p ~/.local/lib
+#case $OSTYPE in
+#    darwin*)
+#        ln -s /usr/local/Cellar/llvm/HEAD-600899c/lib ~/.local/lib/llvm ;;
+#esac
 
